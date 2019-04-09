c11d8b218ee5f3aaaf239f02e9ad1ddbac58ca1d
PapaChapps <Makaychapp@gmail.com>
2019-04-09 10:45:46 -0600

    finished google login

--- Snackii/AppDelegate.swift
+++ Snackii/AppDelegate.swift
@@ -7,22 +7,21 @@
 //
 
 import UIKit
-import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    let firebaseSetup = FirebaseSetup()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        FirebaseApp.configure()
+        firebaseSetup.setupFirebase()
         
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
@@ -45,6 +44,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        // Called when another application tries to open our application
+        return firebaseSetup.handleOpenURL(url: url, options: options)
+    }
 }
-
---
+++ Snackii/FirebaseSetup.swift
@@ -0,0 +1,41 @@
+//
+//  FirebaseSetup.swift
+//  Snackii
+//
+//  Created by Makay Chapple on 4/9/19.
+//  Copyright © 2019 Dallin Smuin. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseUI
+import FirebaseAuth
+
+class FirebaseSetup: NSObject {
+    func setupFirebase() {
+        // 1 Configure Firebase
+        FirebaseApp.configure()
+        
+        // 2 Set the current instance of FirebaseSetup as the delegate of defaultAuthUI.
+        FUIAuth.defaultAuthUI()?.delegate = self
+        
+        // 3 Set defaultAuthUI providers as just GoogleAuth for now.
+        FUIAuth.defaultAuthUI()?.providers = [FUIGoogleAuth()]
+    }
+    
+    func handleOpenURL(url: URL, options: [UIApplication.OpenURLOptionsKey: Any]) -> Bool {
+        // 1 Retrieving a string from options that represents the source application
+        let sourceApplication = options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String
+        
+        // 2 Call handleOpen on defaultAuthUI and return the result and if defaultAuthUI doesn't exist return false.
+        return FUIAuth.defaultAuthUI()?.handleOpen(url, sourceApplication: sourceApplication) ?? false
+    }
+}
+
+// MARK: - FUIAuthDelegate Implementation
+extension FirebaseSetup: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        // Do Something here
+        print(authDataResult)
+    }
+}
