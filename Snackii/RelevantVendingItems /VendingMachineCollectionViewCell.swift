31c22a1e6029bb43fc7c134db414a9e47463dc66
PapaChapps <Makaychapp@gmail.com>
2019-04-19 09:39:57 -0600

    started VendingMachine style storyboard and working on a array of dictionaries representing the individual snack with variables

---
+++ Snackii/RelevantVendingItems /VendingMachineCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  VendingMachineCollectionViewCell.swift
+//  Snackii
+//
+//  Created by Makay Chapple on 4/19/19.
+//  Copyright © 2019 Dallin Smuin. All rights reserved.
+//
+
+import UIKit
+
+class VendingMachineCollectionViewCell: UICollectionViewCell {
+    
+}
