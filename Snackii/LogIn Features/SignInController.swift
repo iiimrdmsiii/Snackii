535c1de6d69bdb797495ff0886f4ec4ac7df326f
iiimrdmsiii <42744179+iiimrdmsiii@users.noreply.github.com>
2019-04-30 10:05:29 -0600

    Organize project (#24)
    * more pods
    
    * finished google login
    
    * commit
    
    * finished google signing method
    
    * Vending images (#6)
    
    * end me please
    
    * testing
    
    * Vending images (#10)
    
    * end me please
    
    * testing
    
    * hello
    
    * asd
    
    * working on have an array of images up to firestorage
    
    * Got the image array to save up to firebase Storage
    
    * updated git ignore file (#11)
    
    * Updated pods
    
    * updating work
    
    * Vending images (#12)
    
    * end me please
    
    * testing
    
    * hello
    
    * asd
    
    * working on have an array of images up to firestorage
    
    * Got the image array to save up to firebase Storage
    
    * still working on delete image from the firebase storage
    
    * updated gitignore
    
    * Update .gitIgnore~
    
    updated gitignore again
    
    * started VendingMachine style storyboard and working on a array of dictionaries representing the individual snack with variables
    
    * completed vending controller and simplified Snack into a protocol which food and drinks conform to.
    
    * wa
    
    * working on vending functions
    
    * convenience commit
    
    * working on vending functions (#14)
    
    * Remove images (#15)
    
    * able to get the Admin working as a test trial
    
    * added xsuserdata
    
    * still working on delete and add imageURL, name and quantity
    
    * Working on delete and others.
    
    * fix merge conflict with project file and storyboard
    
    * removed array of person
    
    * updated git ignore to include xcuserdata
    
    * manage to fix few errors after conflict and added firestorage cloud code (#19)
    
    * Vending machine (#20)
    
    * changed person and added new var called admin which will controll and decide weather or not a user has admin privilages
    
    * building order summery controller
    
    * Fixed problem with conflicts being pushed to remote.
    
    * Fixed merge errors (#22)
    
    * orginze the code and projects

---
+++ Snackii/LogIn Features/SignInController.swift
@@ -0,0 +1,18 @@
+//
+//  SignInController.swift
+//  Snackii
+//
+//  Created by Makay Chapple on 4/15/19.
+//  Copyright © 2019 Dallin Smuin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+import FirebaseUI
+import FirebaseAuth
+
+
+class SignInController {
+
+}
---
+++ Snackii/RelevantVendingItems /VendingMachineCollectionViewController.swift
@@ -0,0 +1,62 @@
+//
+//  VendingMachineCollectionViewController.swift
+//  Snackii
+//
+//  Created by Makay Chapple on 4/19/19.
+//  Copyright © 2019 Dallin Smuin. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class VendingMachineCollectionViewController: UICollectionViewController {
+    var snacks: [Snack] = [Drink(name: "Pepsi", image: "2000px-Pepsi_logo_2014.svg"), Food(name: "Lays", image: "250px-Mid_products_lays.svg")]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        (collectionViewLayout as? UICollectionViewFlowLayout)?.minimumInteritemSpacing = 20
+        (collectionViewLayout as? UICollectionViewFlowLayout)?.minimumLineSpacing = 20
+        collectionView.contentInset = UIEdgeInsets(top: 20, left: 40, bottom: 20, right: 40)
+    
+        (collectionViewLayout as? UICollectionViewFlowLayout)?.itemSize.height = 200
+        (collectionViewLayout as? UICollectionViewFlowLayout)?.itemSize.width = 150
+        
+        self.collectionView.backgroundColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return snacks.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as!
+        VendingMachineCollectionViewCell
+        
+        cell.updateCell(snack: snacks[indexPath.item])
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let snack = snacks[indexPath.item]
+        print(snack.name)
+    }
+
+}
