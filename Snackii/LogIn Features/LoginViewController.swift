f6620cb06db77055b33529c012e8f250e04cb0cc
Hayden Hoki <ghhoki@gmail.com>
2019-05-03 09:13:46 -0600

    Up to date (#31)
    going to confirm this and then have Tyler start working on the firebase log in and sign up method

--- Snackii/LogIn Features/LoginViewController.swift
+++ Snackii/LogIn Features/LoginViewController.swift
@@ -12,12 +12,20 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var emailLoginTextField: UITextField!
     @IBOutlet weak var passwordLoginTextField: UITextField!
+    let email = "bob@bobmail.com"
+    let password = "bob"
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        if emailLoginTextField.text == email && passwordLoginTextField.text == password {
+            performSegue(withIdentifier: "Login", sender: self)
+        }
+    }
     
     @IBAction func signUpButtonPressed(_ sender: Any) {
         performSegue(withIdentifier: "SignUp", sender: self)
