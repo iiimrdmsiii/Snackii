48c00ab153c4771d1f84532ba2c3071720f4b8da
Tyler Donohue <donohue_tyler@yahoo.com>
2019-05-06 01:53:52 -0600

    added dictionary to person

--- Snackii/LogIn Features/Person.swift
+++ Snackii/LogIn Features/Person.swift
@@ -8,10 +8,41 @@
 
 import Foundation
 
-struct Person: Equatable {
+protocol Identifiable {
+    var id: String? { get set }
+}
+
+protocol DocumentUserSerializable {
+    init?(personDictionary: [String: Any])
+}
+
+struct Person {
     let name: String
     let email: String
     let password: String
-    var admin = false
+    var admin: Bool = false
+    let id: String
+    
+    var personDictionary: [String: Any] {
+        return [
+            
+            "name": name,
+            "email": email,
+            "password": password,
+            "adin": admin,
+            "id": id
+        ]
+    }
+    
 }
 
+extension Person {
+    init?(personDictionary: [String : Any]) {
+        guard let name = personDictionary["name"] as? String,
+            let email = personDictionary["email"] as? String,
+            let password = personDictionary["password"] as? String,
+            let admin = personDictionary["admin"] as? Bool,
+            let id = personDictionary["id"] as? String else {return nil}
+        self.init(name: name, email: email, password: password, admin: admin, id: id)
+    }
+}
